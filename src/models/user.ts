c0c8ea0578db6373639409af0971abbcdd976fea
limtis0 <limtis2109@gmail.com>
2025-03-21 18:45:36 +0100

    User model & Direct migrations

---
+++ src/models/user.ts
@@ -0,0 +1,37 @@
+import { DataTypes, Model, Optional } from 'sequelize';
+import { sequelize } from '@/services/sequelize';
+
+interface UserAttributes {
+    id: number;
+    balance: number;
+}
+
+interface UserCreationAttributes extends Optional<UserAttributes, 'id'> { }
+
+export class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
+    public id!: number;
+    public balance!: number;
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        balance: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0,
+            validate: {
+                min: 0,
+            },
+        },
+    },
+    {
+        sequelize,
+        tableName: 'Users',
+        timestamps: false,
+    }
+);
--- src/services/umzug.ts
+++ src/services/umzug.ts
@@ -1,11 +1,18 @@
 import { Umzug, SequelizeStorage } from 'umzug';
 import { sequelize } from '@/services/sequelize';
 import path from 'path';
+import { down as createUserDown, up as createUserUp } from '@/migrations/01-create-user';
+
+console.log(path.join(__dirname, '../migrations/*'));
 
 export const umzug = new Umzug({
-    migrations: {
-        glob: path.join(__dirname, '../migrations/*.ts'),  // TODO: Make path absolute
-    },
+    migrations: [
+        {
+            name: '01-create-user',
+            up: createUserUp,
+            down: createUserDown
+        }
+    ],
     context: sequelize.getQueryInterface(),
     storage: new SequelizeStorage({ sequelize }),
     logger: console,
